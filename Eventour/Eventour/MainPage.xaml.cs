218e7c6321207d765c30dd70c8186cabe457d678
njey123 <njey123@users.noreply.github.com>
2019-03-18 01:03:43 -0400

    Can finally get deserialize JSON response from server

--- Eventour/Eventour/MainPage.xaml.cs
+++ Eventour/Eventour/MainPage.xaml.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xamarin.Forms;
 using RestSharp;
-using Newtonsoft.Json;
+using Xamarin.Forms;
+using Newtonsoft;
+
 
 namespace Eventour
 {
@@ -13,13 +11,11 @@ namespace Eventour
     {
         public class DataDisplay
         {
-            public string Destination { get; set; }
+            public string Dest { get; set; }
             public string StartDate { get; set; }
             public string EndDate { get; set; }
-            // public List<string> Attractions { get; set; }
-            // public List<string> Ratings { get; set; }
-            public string Attraction { get; set; }
-            public string Rating { get; set; }
+            public List<string> Attractions { get; set; }
+            public List<string> Ratings { get; set; }
         }
 
         public MainPage()
@@ -31,48 +27,20 @@ namespace Eventour
         async void OnSuggestButtonClicked(object sender, EventArgs e)
         {
             var client = new RestClient();
-            client.BaseUrl = new Uri("http://127.0.0.1:5000/test");
+            client.BaseUrl = new Uri("http://127.0.0.1:5000/");
 
-            var request = new RestRequest(Method.POST);
+            var request = new RestRequest("test", Method.POST);
             request.AddParameter("dest", destination.Text);
             request.AddParameter("start_date", startDate.Text);
             request.AddParameter("end_date", endDate.Text);
 
-            // request.AddParameter("dest", "Paris, France");
-            // request.AddParameter("start_date", "01/02/2019");
-            // request.AddParameter("end_date", "08/02/2019");
-
             // Send request to server and get response back
             IRestResponse response = client.Execute(request);
-            // outputText.Text = response.Content;
 
-            // // Parse JSON data from server
-            // var results = JsonConvert.DeserializeObject<dynamic>(response.Content);
-            // var dest = results.dest;
-            // var start_date = results.start_date;
-            // var end_date = results.end_date;
-            // var attractions = results.attractions;
-            // var ratings = results.ratings;
+            // Deserialize JSON response from server
+            DataDisplay data = Newtonsoft.Json.JsonConvert.DeserializeObject<DataDisplay>(response.Content);
 
-            // Different way to parse JSON data
-            // List<DataDisplay> data = JsonConvert.DeserializeObject<List<DataDisplay>>(response.Content.ToString());
-            // DataDisplay data = new DataDisplay();
-            DataDisplay data = JsonConvert.DeserializeObject<DataDisplay>(response.Content);
-
-            // // Ensure data from the server can be added to the new screen
-            // App.Destination.Add(dest);
-            // App.Destination.Add(start_date);
-            // App.Destination.Add(end_date);
-            // App.Destination.Add(attractions);
-            // App.Destination.Add(ratings);
-
-            // var searchResultsPage = new SearchResults(dest);
-            // searchResultsPage.BindingContext = dest;
-
-            // await Navigation.PushAsync(new SearchResults((string)data.Destination));
-            await Navigation.PushAsync(new SearchResults("Paris, France", "01/02/2019", "08/02/2019", "Eiffel Tower", "4.6"));
-            // await Navigation.PushAsync(new SearchResults(data.Destination, data.StartDate, data.EndDate, data.Attraction, data.Rating));
-            // await Navigation.PushAsync(searchResultsPage);
+            await Navigation.PushAsync(new SearchResults(data.Dest, data.StartDate, data.EndDate, data.Attractions[0], data.Ratings[0]));
         }
     }
 }
