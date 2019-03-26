2845f34b4e9bee6ec411c8078f408cb305392dc7
njey123 <njey123@users.noreply.github.com>
2019-03-26 13:52:59 -0400

    Add ability to remove an attraction on search results page

--- Eventour/Eventour/SearchResults.xaml.cs
+++ Eventour/Eventour/SearchResults.xaml.cs
@@ -7,6 +7,13 @@ namespace Eventour
 {
     public partial class SearchResults : ContentPage
     {
+        // Create grid for every day in trip and every attraction on every day in trip
+        List<List<Grid>> imgGrids = new List<List<Grid>>();
+        List<List<Grid>> textGrids = new List<List<Grid>>();
+        List<List<ImageButton>> imgBtns = new List<List<ImageButton>>();
+        // List<List<MinusImgBtnDecorator>> minusImgBtnDecorators = new List<List<MinusImgBtnDecorator>>();
+
+
         public SearchResults(string dest, string startDate, string endDate, List<List<string>> attractions, List<List<string>> ratings, List<List<string>> reviewCounts, List<List<string>> imageURLs, List<List<string>> durations, List<List<string>> descriptions, List<List<string>> addresses)
         {
             InitializeComponent();
@@ -19,7 +26,7 @@ namespace Eventour
             endDateResult.Text = (endDateObj).ToString("MMM. dd, yyyy");
 
             // For each day in trip
-            for (var i = 0; i < attractions.Count; i++) {
+            for (int i = 0; i < attractions.Count; i++) {
                 var horizLine = new BoxView { HorizontalOptions = LayoutOptions.FillAndExpand, HeightRequest = 2, BackgroundColor = Color.FromHex("#F5F5F5") };
                 SearchResultsStack.Children.Add(horizLine);
 
@@ -42,21 +49,49 @@ namespace Eventour
                 // Add to stack layout
                 SearchResultsStack.Children.Add(dayGrid);
 
+                // Create new list of grids for current day
+                List<Grid> currDayImgGrid = new List<Grid>();
+                imgGrids.Add(currDayImgGrid);
+                List<Grid> currDayTextGrid = new List<Grid>();
+                textGrids.Add(currDayTextGrid);
+                List<ImageButton> currDayImgBtns = new List<ImageButton>();
+                imgBtns.Add(currDayImgBtns);
+                /* // Create new list of minus image button decorators for current day
+                List<MinusImgBtnDecorator> currDayMinusImgBtnDecorators = new List<MinusImgBtnDecorator>();
+                minusImgBtnDecorators.Add(currDayMinusImgBtnDecorators); */
+
                 // For each attraction planned on current day in trip
-                for (var j = 0; j < attractions[i].Count; j++)
+                for (int j = 0; j < attractions[i].Count; j++)
                 {
                     // Create 4x5 grid for image
                     var imgGrid = new Grid { Padding = new Thickness(30, 20, 30, 20), HorizontalOptions = LayoutOptions.Fill, VerticalOptions = LayoutOptions.Fill };
                     imgGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(135) });
                     imgGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(65) });
                     imgGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+                    currDayImgGrid.Add(imgGrid);
 
                     // Boxviews
                     var currBoxview = new BoxView { CornerRadius = 10, HorizontalOptions = LayoutOptions.Fill, BackgroundColor = Color.FromHex("#72D5E6"), Opacity = 0.8 };
                     // Image
                     var attractionImg = new Image { Source = "https://media-cdn.tripadvisor.com/media/photo-w/0f/38/33/f6/beautiful-day-to-see.jpg", Aspect = Aspect.Fill, HorizontalOptions = LayoutOptions.Fill, VerticalOptions = LayoutOptions.Fill };
-                    // Minus icon
-                    var minusImg = new Image { Source = "Minus.png", Aspect = Aspect.Fill, HorizontalOptions = LayoutOptions.End };
+
+                    // Minus image button
+                    string bindingContextMinusImgBtn = String.Format("{0},{1}", i, j);
+                    var minusImgBtn = new ImageButton { Source = "Minus.png", Aspect = Aspect.Fill, HorizontalOptions = LayoutOptions.End, BindingContext = bindingContextMinusImgBtn};
+                    currDayImgBtns.Add(minusImgBtn);
+                    // Event handler - when click minus button
+                    imgBtns[i][j].Clicked += OnMinusImgBtnClicked;
+
+                    /* // Minus image button decorator
+                    var minusImgBtnDecorator = new MinusImgBtnDecorator
+                    {
+                        DayIndex = i,
+                        ImgGridIndex = j,
+                        MinusImgBtn = minusImgBtn
+                    };
+                    // Add current minus image button decorator to list of minus image button decorators for the day
+                    currDayMinusImgBtnDecorators.Add(minusImgBtnDecorator);
+                    // minusImgBtnDecorators[i][j].MinusImgBtn.Clicked += OnMinusImgBtnClicked; */
 
                     // Frame with rounded corners for image
                     var imgFrame = new Frame { 
@@ -70,7 +105,7 @@ namespace Eventour
                     // Add to image grid
                     imgGrid.Children.Add(currBoxview, 0, 1, 0, 2);
                     imgGrid.Children.Add(imgFrame, 0, 1, 0, 2);
-                    imgGrid.Children.Add(minusImg, 0, 1);
+                    imgGrid.Children.Add(minusImgBtn, 0, 1);
 
                     // Create 3x2 grid for text
                     var grid = new Grid { Padding = new Thickness(30, 0, 30, 30) };
@@ -80,6 +115,7 @@ namespace Eventour
                     grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
                     grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(7, GridUnitType.Star) });
                     grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(5, GridUnitType.Star) });
+                    currDayTextGrid.Add(grid);
 
                     // Heading
                     var attractionHeading = new Label { Text = "Attraction: ", TextColor = Color.Black };
@@ -140,6 +176,34 @@ namespace Eventour
                 }
             }
         }
+
+        // Wrapper (decorator) for minus image button
+        public class MinusImgBtnDecorator
+        {
+            public int DayIndex { get; set; }
+            public int ImgGridIndex { get; set; }
+            public ImageButton MinusImgBtn { get; set; }
+        }
+
+        // Delete attraction on certain day when minus button is clicked
+        void OnMinusImgBtnClicked(object sender, EventArgs e)
+        {
+            // var minusImgBtnDecorator = sender as MinusImgBtnDecorator;
+
+            var imgBtn = sender as ImageButton;
+            string indicesStr = imgBtn.BindingContext as string;
+            string[] indicesStrArr = indicesStr.Split(',');
+            int dayIdx = Int32.Parse(indicesStrArr[0]);
+            int imgGridIdx = Int32.Parse(indicesStrArr[1]);
+
+            /* var dayIdx = minusImgBtnDecorator.DayIndex;
+            var imgGridIdx = minusImgBtnDecorator.ImgGridIndex;*/
+
+            // Remove image and text grids from page
+            SearchResultsStack.Children.Remove(imgGrids[dayIdx][imgGridIdx]);
+            SearchResultsStack.Children.Remove(textGrids[dayIdx][imgGridIdx]);
+        }
+
         async void OnSuggestButtonClicked(object sender, EventArgs e)
         {
             await Navigation.PopAsync();
